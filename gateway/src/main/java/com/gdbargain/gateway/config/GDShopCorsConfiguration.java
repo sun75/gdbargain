13b6c39b54369a87b8813567bc1278b88a54ccb3
finalfrontier <holasunhui@sina.com>
2022-12-30 17:23:25 +0800

    网关pom文件一致性修改+网关及renre-fast跨域配置

---
+++ gateway/src/main/java/com/gdbargain/gateway/config/GDShopCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.gdbargain.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author: shh
+ * @createTime: 2022/12/3016:26
+ */
+@Configuration
+public class GDShopCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
--- product/src/main/java/com/gdbargain/product/ProductApplication.java
+++ product/src/main/java/com/gdbargain/product/ProductApplication.java
@@ -3,8 +3,10 @@ package com.gdbargain.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.gdbargain.product.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductApplication {
 
