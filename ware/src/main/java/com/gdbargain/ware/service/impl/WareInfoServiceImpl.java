25b356d7786fbca4b346f1018e8c355968a41840
finalfrontier <holasunhui@sina.com>
2023-07-26 18:25:31 +0800

    整合ware微服务(初始化需要做的准备工作)，获取ware列表

--- ware/src/main/java/com/gdbargain/ware/service/impl/WareInfoServiceImpl.java
+++ ware/src/main/java/com/gdbargain/ware/service/impl/WareInfoServiceImpl.java
@@ -11,6 +11,7 @@ import com.gdbargain.common.utils.Query;
 import com.gdbargain.ware.dao.WareInfoDao;
 import com.gdbargain.ware.entity.WareInfoEntity;
 import com.gdbargain.ware.service.WareInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareInfoService")
@@ -18,10 +19,18 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if(!StringUtils.isEmpty(key)){
+            wrapper.and((w)->{
+                w.eq("id",key)
+                        .or().like("name",key)
+                        .or().like("address",key)
+                        .or().like("areacode",key);
+            });
+        }
         IPage<WareInfoEntity> page = this.page(
-                new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
-        );
+                new Query<WareInfoEntity>().getPage(params),wrapper);
 
         return new PageUtils(page);
     }
