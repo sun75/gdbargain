48c270af9373467a870fb939a2ad8c2477f8c144
finalfrontier <1123525416@qq.com>
2024-02-21 17:45:49 +0800

    检查

---
+++ gdthird-party/src/main/java/com/gdbargain/gdthirdparty/GdthirdPartyApplication.java
@@ -0,0 +1,16 @@
+package com.gdbargain.gdthirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//取消数据源自动配置
+public class GdthirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdthirdPartyApplication.class, args);
+    }
+
+}
