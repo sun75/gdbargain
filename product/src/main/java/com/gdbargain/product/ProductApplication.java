361eefd88b496c9073302058bb8dfbc8888d472f
finalfrontier <1123525416@qq.com>
2024-09-19 15:05:45 +0800

    test1

--- product/src/main/java/com/gdbargain/product/ProductApplication.java
+++ product/src/main/java/com/gdbargain/product/ProductApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-//test
+//test1
 @EnableCaching
 @EnableFeignClients(basePackages = "com.gdbargain.product.feign")
 @MapperScan("com.gdbargain.product.dao")
